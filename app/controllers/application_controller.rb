c7fcc71b716f43b4b2c5cacc606eef909d652eab
sogo7 <Sogohara7@icloud.com>
2021-01-02 14:47:32 +0900

    ログイン、ログアウト追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,5 @@
 class ApplicationController < ActionController::Base
-
-    def log_in(user)
-        session[:user_id] = user.id
-    end
-    
-    def current_user
-        if session[:user_id]    
-        @current_user ||= User.find_by(id: session[:user_id])
-        pp @current_user
-        pp User.find_by(id: session[:user_id])
-        end
-    end
+    include SessionsHelper
 
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+
   def destroy
+    log_out if logged_in?
+    redirect_to root_path
   end
 end
 
