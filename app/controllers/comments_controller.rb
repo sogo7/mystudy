d161199695f3708314080c0c247f20c51d231f2c
sogo7 <Sogohara7@icloud.com>
2021-01-12 19:14:48 +0900

    コメント機能実装完了！

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,13 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(post_comments_params)
+    @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
       redirect_to request.referer
     else
-      @post_new = Book.new
       @comments = @post.comments
-      redirect_to new_post_path
+      redirect_to show_post_path
     end
   end
 
